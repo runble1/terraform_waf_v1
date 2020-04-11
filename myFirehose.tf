6f04e16185f7b34c80ca6bfc67ece50cc934ccab
0ky3r43m78m070b@gmail.com <0ky3r43m78m090b@gmail.com>
2020-04-11 16:45:01 +0900

    Add CloudWatch for Firehose

--- myFirehose.tf
+++ myFirehose.tf
@@ -1,9 +1,15 @@
+# ## #
+# S3 #
+# ## #
 resource "aws_s3_bucket" "bucket" {
   bucket = "tf-test-bucket-waf"
   acl    = "private"
   region = "us-east-1"
 }
 
+# ####### #
+# Kinesis #
+# ####### #
 resource "aws_iam_role" "firehose_role" {
   name = "firehose_test_role"
 
@@ -31,5 +37,22 @@ resource "aws_kinesis_firehose_delivery_stream" "test_stream" {
   s3_configuration {
     role_arn   = aws_iam_role.firehose_role.arn
     bucket_arn = aws_s3_bucket.bucket.arn
+    cloudwatch_logging_options {
+      enabled         = true
+      log_group_name  = aws_cloudwatch_log_group.cloudwatch_log_group.name
+      log_stream_name = aws_cloudwatch_log_stream.cloudwatch_log_stream.name
+    }
   }
+}
+
+# ############### #
+# CloudWatch Logs #
+# ############### #
+resource "aws_cloudwatch_log_group" "cloudwatch_log_group" {
+  name = "aws-waf-logs-cloudwatch-log-group"
+}
+
+resource "aws_cloudwatch_log_stream" "cloudwatch_log_stream" {
+  name           = "aws-waf-logs-cloudwatch-log-stream"
+  log_group_name = aws_cloudwatch_log_group.cloudwatch_log_group.name
 }
